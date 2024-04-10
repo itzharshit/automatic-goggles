53e5cdf7e7f3e5135efb5b0fefab6eb5af86122f
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2024-04-10 15:29:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def index():
             poem = generate_response(prompt)
     return render_template('index.html', poem=poem)
     
-app.run(host='0.0.0.0', port=8080)
 
-#if __name__ == '__main__':
-#    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
