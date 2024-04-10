6b33b46c98ef820508f8f287673b1e61803a5f56
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2024-04-10 15:26:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,6 +32,8 @@ def index():
             prompt = f'''You are a very good and creative poem writer, write a poem for me on title "{poem_title}", generate this poem in "English" language.'''
             poem = generate_response(prompt)
     return render_template('index.html', poem=poem)
+    
+app.run(host='0.0.0.0', port=8080)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
