4d518f785ce04048b8883af1857308fcde9e51ce
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2024-04-10 19:36:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 from flask import Flask, render_template, request
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+import os
 
 app = Flask(__name__)
 
-OPENAI_API_KEY = "API_KEY_HERE"
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 def generate_response(input_text):
-    llm = OpenAI(temperature=0.7, openai_api_key=OPENAI_API_KEY)
-    return llm(input_text)
-
+    try:
+        llm = OpenAI(temperature=0.7, openai_api_key=OPENAI_API_KEY)
+        return llm(input_text)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+try:
+    print(generate_response("Are you there?"))
+except Exception as e:
+    print(f"An error occurred: {e}")
+    
 @app.route('/', methods=['GET', 'POST'])
 def index():
     poem = None
     if request.method == 'POST':
-        poem_title = request.form['poem_title']
-        lang = request.form['language']
-        prompt = f'''You are a very good and creative poem writer, write a poem for me on title "{poem_title}", generate this poem in "{lang}" language.'''
-        poem = generate_response(prompt)
+        poem_title = request.form.get('poem_title', '')
+        lang = request.form.get('language', '')
+        if lang:
+            prompt = f'''You are a very good and creative poem writer, write a poem for me on title "{poem_title}", generate this poem in "{lang}" language. Please note if you can't generate the poem on given topic or you can'nt understand the given topic then simply say "I can't do it" nothing else or any other excuse.'''
+            poem = generate_response(prompt)
+        else:
+            prompt = f'''You are a very good and creative poem writer, write a poem for me on title "{poem_title}", generate this poem in "English" language.Please note if you can't generate the poem on given topic or you can'nt understand the given topic then simply say "I can't do it" nothing else or any other excuse.'''
+            poem = generate_response(prompt)
     return render_template('index.html', poem=poem)
-
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=500, debug=True)
