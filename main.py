f963c983967acfd8c22429187bdb7033adcbc8a5
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2024-04-10 14:49:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
+# app.py
 from flask import Flask, render_template, request
 from langchain.llms import OpenAI
+import os
 
 app = Flask(__name__)
 
-OPENAI_API_KEY = "API_KEY_HERE"
+OPENAI_API_KEY = ""
 
 def generate_response(input_text):
-    llm = OpenAI(temperature=0.7, openai_api_key=OPENAI_API_KEY)
-    return llm(input_text)
+    try:
+        llm = OpenAI(temperature=0.7, openai_api_key=OPENAI_API_KEY)
+        return llm(input_text)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     poem = None
     if request.method == 'POST':
-        poem_title = request.form['poem_title']
-        lang = request.form['language']
-        prompt = f'''You are a very good and creative poem writer, write a poem for me on title "{poem_title}", generate this poem in "{lang}" language.'''
-        poem = generate_response(prompt)
+        poem_title = request.form.get('poem_title', '')
+        lang = request.form.get('language', '')
+        if lang:
+            prompt = f'''You are a very good and creative poem writer, write a poem for me on title "{poem_title}", generate this poem in "{lang}" language.'''
+            poem = generate_response(prompt)
+        else:
+            prompt = f'''You are a very good and creative poem writer, write a poem for me on title "{poem_title}", generate this poem in "English" language.'''
+            poem = generate_response(prompt)
     return render_template('index.html', poem=poem)
 
 if __name__ == '__main__':
